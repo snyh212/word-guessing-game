9ad5915e6f71319d827adca314ccf9d4df683077
Brice Veyna <bricev99@gmail.com>
2022-07-19 14:34:36 -0700

    Add some stuff

--- Develop/lib/Letter.js
+++ Develop/lib/Letter.js
@@ -1,5 +1,17 @@
 class Letter {
-  
+  constructor(letter) {
+    this.letter = letter;
+
+    const letterList = "abcdefghijklmnopqrstuvwxyz0123456789";
+    
+    if (letterList.includes(letter)) {
+        
+    }
+  }
+
+
 }
+
+
 
 module.exports = Letter;
--- Develop/lib/Word.js
+++ Develop/lib/Word.js
@@ -1,7 +1,15 @@
 const Letter = require("./Letter");
 
 class Word {
-  
+  constructor(randomWord) {
+    const letterList = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < randomWord.length; i++) {
+        if (letterList.includes(randomWord[i])) {
+            randomWord[i] = "_";
+        }
+    }
+  }
 }
 
 module.exports = Word;
